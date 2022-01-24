414505a08d8e4d7be4bdb21e56198746c7ca5fbc
Francis <92971421+francis816@users.noreply.github.com>
2022-01-24 11:24:24 -0600

    3C: backend connect to db, db in route handlers

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
+// so Note = module.exports = mongoose.model("Note", noteSchema)
+const Note = require('./models/note')
 
 
 app.use(express.static('build'))
@@ -47,25 +50,35 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-    // when clients request the notes page, we send them a page with all notes in json format
-    response.json(notes)
+    // fetch all notes from db instead of server
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
+
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    // when clients request a specific note page, we try to find the specific note page according to its id
-    // convert string to number
-    const id = Number(request.params.id)
-    // we try to find the note by matching the id from client with the id from our server that contains the note
-    const note = notes.find(note => {
-        return note.id === id
-    })
-    if (note) {
-        // we end them a page with the specific note in json format
+    // // when clients request a specific note page, we try to find the specific note page according to its id
+    // // convert string to number
+    // const id = Number(request.params.id)
+    // // we try to find the note by matching the id from client with the id from our server that contains the note
+    // const note = notes.find(note => {
+    //     return note.id === id
+    // })
+    // if (note) {
+    //     // we end them a page with the specific note in json format
+    //     response.json(note)
+    // } else {
+    //     //e.g. out of length, set code to 404, end without sending any data        
+    //     response.status(404).end()
+    // }
+
+
+    // above = get notes server,
+    // now we connect to db, Mongoose's findById method
+    Note.findById(request.params.id).then(note => {
         response.json(note)
-    } else {
-        //e.g. out of length, set code to 404, end without sending any data        
-        response.status(404).end()
-    }
+    })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -94,20 +107,22 @@ app.post('/api/notes', (request, response) => {
         })
     }
 
-    const note = {
+    // add to Note in db rather than server
+    const note = new Note({
         content: body.content,
         // = body.important if body.important has a value. Otherwise = false
         important: body.important || false,
         date: new Date(),
         id: generateId(),
-    }
-
-    notes = notes.concat(note)
-
-    response.json(note)
+    })
+    //The note objects are created with the Note constructor function. The response is sent inside of the callback function for the save operation. 
+    //This ensures that the response is sent only if the operation succeeded. 
+    note.save().then(savedNote => {
+        response.json(note)
+    })
 })
 
-// dynamic port for Heroku or 3001 if former is not defined
+// dynamic port for Heroku or according to .env flie, otherwise 3001 if former is not defined
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
---
+++ part3/models/note.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+// process.env.MONGODB_URI = MONGODB_URI in .env file
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+
+mongoose.connect(url)
+    .then(result => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('error connecting to MongoDB:', error.message)
+    })
+
+const noteSchema = new mongoose.Schema({
+    content: String,
+    date: Date,
+    important: Boolean,
+})
+
+
+// remove _id and __v obj property when go to http://localhost:3001/api/notes
+noteSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Note', noteSchema)
