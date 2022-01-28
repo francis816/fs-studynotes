3bf76750dbd820412b0f5e18dea7940242a0ce61
Francis <92971421+francis816@users.noreply.github.com>
2022-01-28 15:05:43 -0600

    Update note.js

--- part4/models/note.js
+++ part4/models/note.js
@@ -1,18 +1,5 @@
 const mongoose = require('mongoose')
 
-// process.env.MONGODB_URI = MONGODB_URI in .env file
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-
-mongoose.connect(url)
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
