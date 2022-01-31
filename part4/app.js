06e9ad4e6281a9f46391dd214019a0599a069f4f
Francis <92971421+francis816@users.noreply.github.com>
2022-01-31 12:55:44 -0600

    fix bug

--- part4/app.js
+++ part4/app.js
@@ -6,6 +6,19 @@ const notesRouter = require('./controllers/notes')
 const middleware = require('./utils/middleware')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+    .then(() => {
+        logger.info('connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.error('error connecting to MongoDB:', error.message)
+    })
+
 
 
 app.use(cors())
